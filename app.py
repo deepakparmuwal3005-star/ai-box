c90d03d004c4962ff512604b8ef973fdb6330dd5
deepakparmuwal3005-star <deepakparmuwal3005@gmail.com>
2026-08-17 01:34:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,7 @@ else:
                 try:
                     # Configure Gemini
                     genai.configure(api_key=api_key)
-                    model = genai.GenerativeModel('gemini-1.5-flash')
-                    
+                    model = genai.GenerativeModel('gemini-2.5-flash')
                     prompt = f"""
                     Aap ek expert E-commerce Copywriter aur SEO specialist hain.
                     Product Name: {product_name}
